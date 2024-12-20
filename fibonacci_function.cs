188db14a7284344dd799253f8d27b46ff2165e4b
Behrad barzegar <eman.mehrabi1383@gmail.com>
2024-12-20 22:27:49 +0330

    Create fibonacci_function.cs

---
+++ fibonacci_function.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fibonacci_recursive_function
+{
+//create by emanKapak
+    class Program
+    {
+          static int fibonacci(int n)
+        {
+            if (n==0)
+            {
+                return 0;
+            }
+            else if (n==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return fibonacci(n - 1) + fibonacci(n - 2); 
+            }
+          
+        }
+        static void Main(string[] args)
+        {
+            int n = 0;
+            Console.Write("plesase enter number:");
+            n = int.Parse(Console.ReadLine());
+            Console.WriteLine("fibonaci is :{0}",fibonacci(n));
+            Console.ReadKey();
+        }
+    }
+}
